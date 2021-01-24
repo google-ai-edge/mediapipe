23ac13b285864ebfd720324b24fb974e1b186341
homuler <eulerdora.1810.mann@gmail.com>
2021-01-24 12:05:05 +0900

    use absl::SleepFor instead of usleep

--- mediapipe/util/tracking/box_tracker.cc
+++ mediapipe/util/tracking/box_tracker.cc
@@ -667,7 +667,7 @@ bool BoxTracker::WaitForChunkFile(int id, int checkpoint,
       }
     }
 
-    usleep(wait_time_msec * 1000);
+    absl::SleepFor(absl::Milliseconds(wait_time_msec));
     total_wait_msec += wait_time_msec;
 
     struct stat tmp;
