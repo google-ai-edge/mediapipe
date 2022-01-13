5361430648926e18a68928562f8244573b2efce5
Maksym Walczak <maksym@nimagna.com>
2022-01-13 14:16:54 +0100

    Add copyright note

--- mediapipe/pose_tracking_dll/pose_tracking.h
+++ mediapipe/pose_tracking_dll/pose_tracking.h
@@ -1,3 +1,19 @@
+/**
+   Copyright 2022, Nimagna AG
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+
 #ifndef POSE_TRACKING_LIBRARY_H
 #define POSE_TRACKING_LIBRARY_H
 
@@ -48,7 +64,7 @@ struct Mat {
 
 class DLLEXPORT PoseTracking {
  public:
-  static constexpr size_t landmarksCount = 33u;
+  static constexpr size_t kLandmarksCount = 33u;
   enum LandmarkNames {
     NOSE = 0,
     LEFT_EYE_INNER,
@@ -83,7 +99,7 @@ class DLLEXPORT PoseTracking {
     RIGHT_HEEL,
     LEFT_FOOT_INDEX,
     RIGHT_FOOT_INDEX,
-    COUNT = landmarksCount
+    COUNT = kLandmarksCount
   };
 
   PoseTracking(const char* calculatorGraphConfigFile);
