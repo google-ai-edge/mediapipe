cb52432159ecfea69c6df54be2cb56fd569f275f
kinaryml <kinar.ravishankar@ymedialabs.com>
2022-09-08 06:23:03 -0700

    Added image classification implementation files and associated tests

---
+++ mediapipe/tasks/python/test/vision/image_classification_test.py
@@ -0,0 +1,301 @@
+# Copyright 2022 The MediaPipe Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Tests for image classifier."""
+
+import enum
+
+from absl.testing import absltest
+from absl.testing import parameterized
+
+from mediapipe.python._framework_bindings import image as image_module
+from mediapipe.tasks.python.components.containers import category as category_module
+from mediapipe.tasks.python.components.containers import classifications as classifications_module
+from mediapipe.tasks.python.core import base_options as base_options_module
+from mediapipe.tasks.python.test import test_util
+from mediapipe.tasks.python.vision import image_classification
+from mediapipe.tasks.python.vision.core import vision_task_running_mode as running_mode_module
+
+_BaseOptions = base_options_module.BaseOptions
+_Category = category_module.Category
+_ClassificationEntry = classifications_module.ClassificationEntry
+_Classifications = classifications_module.Classifications
+_ClassificationResult = classifications_module.ClassificationResult
+_Image = image_module.Image
+_ImageClassifier = image_classification.ImageClassifier
+_ImageClassifierOptions = image_classification.ImageClassifierOptions
+_RUNNING_MODE = running_mode_module.VisionTaskRunningMode
+
+_MODEL_FILE = 'mobilenet_v2_1.0_224.tflite'
+_IMAGE_FILE = 'burger.jpg'
+_EXPECTED_CLASSIFICATION_RESULT = _ClassificationResult(
+  classifications=[
+    _Classifications(
+      entries=[
+        _ClassificationEntry(
+          categories=[
+            _Category(
+              index=934,
+              score=0.7952049970626831,
+              display_name='',
+              category_name='cheeseburger'),
+            _Category(
+              index=932,
+              score=0.02732999622821808,
+              display_name='',
+              category_name='bagel'),
+            _Category(
+              index=925,
+              score=0.01933487318456173,
+              display_name='',
+              category_name='guacamole'),
+            _Category(
+              index=963,
+              score=0.006279350258409977,
+              display_name='',
+              category_name='meat loaf')
+          ],
+          timestamp_ms=0
+        )
+      ],
+      head_index=0,
+      head_name='probability')
+  ])
+_ALLOW_LIST = ['cheeseburger', 'guacamole']
+_DENY_LIST = ['cheeseburger']
+_SCORE_THRESHOLD = 0.5
+_MAX_RESULTS = 3
+
+
+class ModelFileType(enum.Enum):
+  FILE_CONTENT = 1
+  FILE_NAME = 2
+
+
+class ImageClassifierTest(parameterized.TestCase):
+
+  def setUp(self):
+    super().setUp()
+    self.test_image = test_util.read_test_image(
+        test_util.get_test_data_path(_IMAGE_FILE))
+    self.model_path = test_util.get_test_data_path(_MODEL_FILE)
+
+  def test_create_from_file_succeeds_with_valid_model_path(self):
+    # Creates with default option and valid model file successfully.
+    with _ImageClassifier.create_from_model_path(self.model_path) as classifier:
+      self.assertIsInstance(classifier, _ImageClassifier)
+
+  def test_create_from_options_succeeds_with_valid_model_path(self):
+    # Creates with options containing model file successfully.
+    base_options = _BaseOptions(file_name=self.model_path)
+    options = _ImageClassifierOptions(base_options=base_options)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      self.assertIsInstance(classifier, _ImageClassifier)
+
+  def test_create_from_options_fails_with_invalid_model_path(self):
+    # Invalid empty model path.
+    with self.assertRaisesRegex(
+        ValueError,
+        r"ExternalFile must specify at least one of 'file_content', "
+        r"'file_name' or 'file_descriptor_meta'."):
+      base_options = _BaseOptions(file_name='')
+      options = _ImageClassifierOptions(base_options=base_options)
+      _ImageClassifier.create_from_options(options)
+
+  def test_create_from_options_succeeds_with_valid_model_content(self):
+    # Creates with options containing model content successfully.
+    with open(self.model_path, 'rb') as f:
+      base_options = _BaseOptions(file_content=f.read())
+      options = _ImageClassifierOptions(base_options=base_options)
+      classifier = _ImageClassifier.create_from_options(options)
+      self.assertIsInstance(classifier, _ImageClassifier)
+
+  @parameterized.parameters(
+      (ModelFileType.FILE_NAME, 4, _EXPECTED_CLASSIFICATION_RESULT),
+      (ModelFileType.FILE_CONTENT, 4, _EXPECTED_CLASSIFICATION_RESULT))
+  def test_classify(self, model_file_type, max_results,
+                  expected_classification_result):
+    # Creates classifier.
+    if model_file_type is ModelFileType.FILE_NAME:
+      base_options = _BaseOptions(file_name=self.model_path)
+    elif model_file_type is ModelFileType.FILE_CONTENT:
+      with open(self.model_path, 'rb') as f:
+        model_content = f.read()
+      base_options = _BaseOptions(file_content=model_content)
+    else:
+      # Should never happen
+      raise ValueError('model_file_type is invalid.')
+
+    options = _ImageClassifierOptions(
+        base_options=base_options, max_results=max_results)
+    classifier = _ImageClassifier.create_from_options(options)
+
+    # Performs image classification on the input.
+    image_result = classifier.classify(self.test_image)
+    # Comparing results.
+    self.assertEqual(image_result, expected_classification_result)
+    # Closes the classifier explicitly when the classifier is not used in
+    # a context.
+    classifier.close()
+
+  @parameterized.parameters(
+      (ModelFileType.FILE_NAME, 4, _EXPECTED_CLASSIFICATION_RESULT),
+      (ModelFileType.FILE_CONTENT, 4, _EXPECTED_CLASSIFICATION_RESULT))
+  def test_classify_in_context(self, model_file_type, max_results,
+                             expected_classification_result):
+    if model_file_type is ModelFileType.FILE_NAME:
+      base_options = _BaseOptions(file_name=self.model_path)
+    elif model_file_type is ModelFileType.FILE_CONTENT:
+      with open(self.model_path, 'rb') as f:
+        model_content = f.read()
+      base_options = _BaseOptions(file_content=model_content)
+    else:
+      # Should never happen
+      raise ValueError('model_file_type is invalid.')
+
+    options = _ImageClassifierOptions(
+        base_options=base_options, max_results=max_results)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      # Performs object detection on the input.
+      image_result = classifier.classify(self.test_image)
+      # Comparing results.
+      self.assertEqual(image_result, expected_classification_result)
+
+  def test_score_threshold_option(self):
+    options = _ImageClassifierOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        score_threshold=_SCORE_THRESHOLD)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      # Performs image classification on the input.
+      image_result = classifier.classify(self.test_image)
+      classifications = image_result.classifications
+
+      for classification in classifications:
+        for entry in classification.entries:
+          score = entry.categories[0].score
+          self.assertGreaterEqual(
+              score, _SCORE_THRESHOLD,
+              f'Classification with score lower than threshold found. '
+              f'{classification}')
+
+  def test_max_results_option(self):
+    options = _ImageClassifierOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        max_results=_MAX_RESULTS)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      # Performs image classification on the input.
+      image_result = classifier.classify(self.test_image)
+      categories = image_result.classifications[0].entries[0].categories
+
+      self.assertLessEqual(
+          len(categories), _MAX_RESULTS, 'Too many results returned.')
+
+  def test_allow_list_option(self):
+    options = _ImageClassifierOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        category_allowlist=_ALLOW_LIST)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      # Performs image classification on the input.
+      image_result = classifier.classify(self.test_image)
+      classifications = image_result.classifications
+
+      for classification in classifications:
+        for entry in classification.entries:
+          label = entry.categories[0].category_name
+          self.assertIn(label, _ALLOW_LIST,
+                        f'Label {label} found but not in label allow list')
+
+  def test_deny_list_option(self):
+    options = _ImageClassifierOptions(
+      base_options=_BaseOptions(file_name=self.model_path),
+      category_denylist=_DENY_LIST)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      # Performs image classification on the input.
+      image_result = classifier.classify(self.test_image)
+      classifications = image_result.classifications
+
+      for classification in classifications:
+        for entry in classification.entries:
+          label = entry.categories[0].category_name
+          self.assertNotIn(label, _DENY_LIST,
+                        f'Label {label} found but in deny list.')
+
+  def test_combined_allowlist_and_denylist(self):
+    # Fails with combined allowlist and denylist
+    with self.assertRaisesRegex(
+        ValueError,
+        r'`category_allowlist` and `category_denylist` are mutually '
+        r'exclusive options.'):
+      options = _ImageClassifierOptions(
+          base_options=_BaseOptions(file_name=self.model_path),
+          category_allowlist=['foo'],
+          category_denylist=['bar'])
+      with _ImageClassifier.create_from_options(options) as unused_classifier:
+        pass
+
+  def test_empty_classification_outputs(self):
+    options = _ImageClassifierOptions(
+        base_options=_BaseOptions(file_name=self.model_path), score_threshold=1)
+    with _ImageClassifier.create_from_options(options) as classifier:
+      # Performs image classification on the input.
+      image_result = classifier.classify(self.test_image)
+      self.assertEmpty(image_result.classifications[0].entries[0].categories)
+
+  def test_missing_result_callback(self):
+    options = _ImageClassifierOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        running_mode=_RUNNING_MODE.LIVE_STREAM)
+    with self.assertRaisesRegex(ValueError,
+                                r'result callback must be provided'):
+      with _ImageClassifier.create_from_options(options) as unused_classifier:
+        pass
+
+  @parameterized.parameters((_RUNNING_MODE.IMAGE), (_RUNNING_MODE.VIDEO))
+  def test_illegal_result_callback(self, running_mode):
+
+    def pass_through(unused_result: _ClassificationResult):
+      pass
+
+    options = _ImageClassifierOptions(
+        base_options=_BaseOptions(file_name=self.model_path),
+        running_mode=running_mode,
+        result_callback=pass_through)
+    with self.assertRaisesRegex(ValueError,
+                                r'result callback should not be provided'):
+      with _ImageClassifier.create_from_options(options) as unused_classifier:
+        pass
+
+  # @parameterized.parameters((0, _EXPECTED_CLASSIFICATION_RESULT),
+  #                           (1, _ClassificationResult(classifications=[])))
+  # def test_classify_async_calls(self, threshold, expected_result):
+  #   observed_timestamp_ms = -1
+  #
+  #   def check_result(result: _ClassificationResult, timestamp_ms: int):
+  #     self.assertEqual(result, expected_result)
+  #     self.assertLess(observed_timestamp_ms, timestamp_ms)
+  #     self.observed_timestamp_ms = timestamp_ms
+  #
+  #   options = _ImageClassifierOptions(
+  #       base_options=_BaseOptions(file_name=self.model_path),
+  #       running_mode=_RUNNING_MODE.LIVE_STREAM,
+  #       max_results=4,
+  #       score_threshold=threshold,
+  #       result_callback=check_result)
+  #   classifier = _ImageClassifier.create_from_options(options)
+  #   for timestamp in range(0, 300, 30):
+  #     classifier.classify_async(self.test_image, timestamp)
+  #   classifier.close()
+
+
+if __name__ == '__main__':
+  absltest.main()
---
+++ mediapipe/tasks/python/vision/image_classification.py
@@ -0,0 +1,227 @@
+# Copyright 2022 The MediaPipe Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""MediaPipe image classifier task."""
+
+import dataclasses
+from typing import Callable, List, Mapping, Optional
+
+from mediapipe.python import packet_creator
+from mediapipe.python import packet_getter
+from mediapipe.python._framework_bindings import image as image_module
+from mediapipe.python._framework_bindings import packet as packet_module
+from mediapipe.python._framework_bindings import task_runner as task_runner_module
+from mediapipe.tasks.cc.components import classifier_options_pb2
+from mediapipe.tasks.cc.vision.image_classification import image_classifier_options_pb2
+from mediapipe.tasks.python.components.containers import classifications as classifications_module
+from mediapipe.tasks.python.core import base_options as base_options_module
+from mediapipe.tasks.python.core import task_info as task_info_module
+from mediapipe.tasks.python.core.optional_dependencies import doc_controls
+from mediapipe.tasks.python.vision.core import base_vision_task_api
+from mediapipe.tasks.python.vision.core import vision_task_running_mode as running_mode_module
+
+_BaseOptions = base_options_module.BaseOptions
+_ClassifierOptionsProto = classifier_options_pb2.ClassifierOptions
+_ImageClassifierOptionsProto = image_classifier_options_pb2.ImageClassifierOptions
+_RunningMode = running_mode_module.VisionTaskRunningMode
+_TaskInfo = task_info_module.TaskInfo
+_TaskRunner = task_runner_module.TaskRunner
+
+_CLASSIFICATION_RESULT_OUT_STREAM_NAME = 'classification_result_out'
+_CLASSIFICATION_RESULT_TAG = 'CLASSIFICATION_RESULT'
+_IMAGE_IN_STREAM_NAME = 'image_in'
+_IMAGE_TAG = 'IMAGE'
+_TASK_GRAPH_NAME = 'mediapipe.tasks.vision.ImageClassifierGraph'
+
+
+@dataclasses.dataclass
+class ImageClassifierOptions:
+  """Options for the image classifier task.
+
+  Attributes:
+    base_options: Base options for the image classifier task.
+    running_mode: The running mode of the task. Default to the image mode.
+      Image classifier task has three running modes:
+      1) The image mode for classifying objects on single image inputs.
+      2) The video mode for classifying objects on the decoded frames of a
+         video.
+      3) The live stream mode for classifying objects on a live stream of input
+         data, such as from camera.
+    display_names_locale: The locale to use for display names specified through
+      the TFLite Model Metadata.
+    max_results: The maximum number of top-scored classification results to
+      return.
+    score_threshold: Overrides the ones provided in the model metadata. Results
+      below this value are rejected.
+    category_allowlist: Allowlist of category names. If non-empty, detection
+      results whose category name is not in this set will be filtered out.
+      Duplicate or unknown category names are ignored. Mutually exclusive with
+      `category_denylist`.
+    category_denylist: Denylist of category names. If non-empty, detection
+      results whose category name is in this set will be filtered out. Duplicate
+      or unknown category names are ignored. Mutually exclusive with
+      `category_allowlist`.
+    result_callback: The user-defined result callback for processing live stream
+      data. The result callback should only be specified when the running mode
+      is set to the live stream mode.
+  """
+  base_options: _BaseOptions
+  running_mode: _RunningMode = _RunningMode.IMAGE
+  display_names_locale: Optional[str] = None
+  max_results: Optional[int] = None
+  score_threshold: Optional[float] = None
+  category_allowlist: Optional[List[str]] = None
+  category_denylist: Optional[List[str]] = None
+  result_callback: Optional[
+      Callable[[classifications_module.ClassificationResult], None]] = None
+
+  @doc_controls.do_not_generate_docs
+  def to_pb2(self) -> _ImageClassifierOptionsProto:
+    """Generates an ImageClassifierOptions protobuf object."""
+    base_options_proto = self.base_options.to_pb2()
+    base_options_proto.use_stream_mode = False if self.running_mode == _RunningMode.IMAGE else True
+
+    classifier_options_proto = _ClassifierOptionsProto(
+        display_names_locale=self.display_names_locale,
+        max_results=self.max_results,
+        score_threshold=self.score_threshold,
+        category_allowlist=self.category_allowlist,
+        category_denylist=self.category_denylist)
+
+    return _ImageClassifierOptionsProto(
+        base_options=base_options_proto,
+        classifier_options=classifier_options_proto
+    )
+
+
+class ImageClassifier(base_vision_task_api.BaseVisionTaskApi):
+  """Class that performs image classification on images."""
+
+  @classmethod
+  def create_from_model_path(cls, model_path: str) -> 'ImageClassifier':
+    """Creates an `ImageClassifier` object from a TensorFlow Lite model and the default `ImageClassifierOptions`.
+
+    Note that the created `ImageClassifier` instance is in image mode, for
+    detecting objects on single image inputs.
+
+    Args:
+      model_path: Path to the model.
+
+    Returns:
+      `ImageClassifier` object that's created from the model file and the default
+      `ImageClassifierOptions`.
+
+    Raises:
+      ValueError: If failed to create `ImageClassifier` object from the provided
+        file such as invalid file path.
+      RuntimeError: If other types of error occurred.
+    """
+    base_options = _BaseOptions(file_name=model_path)
+    options = ImageClassifierOptions(
+        base_options=base_options, running_mode=_RunningMode.IMAGE)
+    return cls.create_from_options(options)
+
+  @classmethod
+  def create_from_options(cls,
+                          options: ImageClassifierOptions) -> 'ImageClassifier':
+    """Creates the `ImageClassifier` object from image classifier options.
+
+    Args:
+      options: Options for the image classifier task.
+
+    Returns:
+      `ImageClassifier` object that's created from `options`.
+
+    Raises:
+      ValueError: If failed to create `ImageClassifier` object from
+        `ImageClassifierOptions` such as missing the model.
+      RuntimeError: If other types of error occurred.
+    """
+
+    def packets_callback(output_packets: Mapping[str, packet_module.Packet]):
+      classification_result_proto = packet_getter.get_proto(
+          output_packets[_CLASSIFICATION_RESULT_OUT_STREAM_NAME])
+
+      classification_result = classifications_module.ClassificationResult([
+          classifications_module.Classifications.create_from_pb2(classification)
+          for classification in classification_result_proto.classifications
+      ])
+      options.result_callback(classification_result)
+
+    task_info = _TaskInfo(
+        task_graph=_TASK_GRAPH_NAME,
+        input_streams=[':'.join([_IMAGE_TAG, _IMAGE_IN_STREAM_NAME])],
+        output_streams=[
+            ':'.join([_CLASSIFICATION_RESULT_TAG,
+                      _CLASSIFICATION_RESULT_OUT_STREAM_NAME])
+        ],
+        task_options=options)
+    return cls(
+        task_info.generate_graph_config(
+            enable_flow_limiting=options.running_mode ==
+            _RunningMode.LIVE_STREAM), options.running_mode,
+        packets_callback if options.result_callback else None)
+
+  # TODO: Create an Image class for MediaPipe Tasks.
+  def classify(
+      self,
+      image: image_module.Image
+  ) -> classifications_module.ClassificationResult:
+    """Performs image classification on the provided MediaPipe Image.
+
+    Args:
+      image: MediaPipe Image.
+
+    Returns:
+      A classification result object that contains a list of classifications.
+
+    Raises:
+      ValueError: If any of the input arguments is invalid.
+      RuntimeError: If image classification failed to run.
+    """
+    output_packets = self._process_image_data(
+        {_IMAGE_IN_STREAM_NAME: packet_creator.create_image(image)})
+    classification_result_proto = packet_getter.get_proto(
+        output_packets[_CLASSIFICATION_RESULT_OUT_STREAM_NAME])
+
+    return classifications_module.ClassificationResult([
+      classifications_module.Classifications.create_from_pb2(classification)
+      for classification in classification_result_proto.classifications
+    ])
+
+  def classify_async(self, image: image_module.Image, timestamp_ms: int) -> None:
+    """Sends live image data (an Image with a unique timestamp) to perform image
+     classification.
+
+    This method will return immediately after the input image is accepted. The
+    results will be available via the `result_callback` provided in the
+    `ImageClassifierOptions`. The `detect_async` method is designed to process
+    live stream data such as camera input. To lower the overall latency, image
+    classifier may drop the input images if needed. In other words, it's not
+    guaranteed to have output per input image. The `result_callback` provides:
+      - A classification result object that contains a list of classifications.
+      - The input image that the image classifier runs on.
+      - The input timestamp in milliseconds.
+
+    Args:
+      image: MediaPipe Image.
+      timestamp_ms: The timestamp of the input image in milliseconds.
+
+    Raises:
+      ValueError: If the current input timestamp is smaller than what the image
+        classifier has already processed.
+    """
+    self._send_live_stream_data({
+        _IMAGE_IN_STREAM_NAME:
+            packet_creator.create_image(image).at(timestamp_ms)
+    })
