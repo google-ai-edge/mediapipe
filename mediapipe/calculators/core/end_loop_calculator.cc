4717ac298c90bb8889fa5a12743eccf109f8a5e0
Jiuqiang Tang <jqtang@google.com>
2022-10-31 11:03:49 -0700

    Add EndLoopMatrixCalculator
    PiperOrigin-RevId: 485097386

--- mediapipe/calculators/core/end_loop_calculator.cc
+++ mediapipe/calculators/core/end_loop_calculator.cc
@@ -19,6 +19,7 @@
 #include "mediapipe/framework/formats/classification.pb.h"
 #include "mediapipe/framework/formats/detection.pb.h"
 #include "mediapipe/framework/formats/landmark.pb.h"
+#include "mediapipe/framework/formats/matrix.h"
 #include "mediapipe/framework/formats/rect.pb.h"
 #include "mediapipe/util/render_data.pb.h"
 #include "tensorflow/lite/interpreter.h"
@@ -57,5 +58,8 @@ REGISTER_CALCULATOR(EndLoopTensorCalculator);
 typedef EndLoopCalculator<std::vector<::mediapipe::Detection>>
     EndLoopDetectionCalculator;
 REGISTER_CALCULATOR(EndLoopDetectionCalculator);
+
+typedef EndLoopCalculator<std::vector<Matrix>> EndLoopMatrixCalculator;
+REGISTER_CALCULATOR(EndLoopMatrixCalculator);
 
 }  // namespace mediapipe
