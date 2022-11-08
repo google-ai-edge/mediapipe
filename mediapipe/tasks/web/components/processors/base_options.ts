26066787b316b57ab4185690ade1a9ac75cbfe81
Sebastian Schmidt <mrschmidt@google.com>
2022-11-08 06:35:43 -0800

    Allow Web developers to opt into CPU or GPU processing
    PiperOrigin-RevId: 486935157

--- mediapipe/tasks/web/components/processors/base_options.ts
+++ mediapipe/tasks/web/components/processors/base_options.ts
@@ -14,6 +14,8 @@
  * limitations under the License.
  */
 
+import {InferenceCalculatorOptions} from '../../../../calculators/tensor/inference_calculator_pb';
+import {Acceleration} from '../../../../tasks/cc/core/proto/acceleration_pb';
 import {BaseOptions as BaseOptionsProto} from '../../../../tasks/cc/core/proto/base_options_pb';
 import {ExternalFile} from '../../../../tasks/cc/core/proto/external_file_pb';
 import {BaseOptions} from '../../../../tasks/web/core/base_options';
@@ -25,26 +27,60 @@ import {BaseOptions} from '../../../../tasks/web/core/base_options';
  * Converts a BaseOptions API object to its Protobuf representation.
  * @throws If neither a model assset path or buffer is provided
  */
-export async function convertBaseOptionsToProto(baseOptions: BaseOptions):
-    Promise<BaseOptionsProto> {
-  if (baseOptions.modelAssetPath && baseOptions.modelAssetBuffer) {
-    throw new Error(
-        'Cannot set both baseOptions.modelAssetPath and baseOptions.modelAssetBuffer');
+export async function convertBaseOptionsToProto(
+    updatedOptions: BaseOptions,
+    currentOptions?: BaseOptionsProto): Promise<BaseOptionsProto> {
+  const result =
+      currentOptions ? currentOptions.clone() : new BaseOptionsProto();
+
+  await configureExternalFile(updatedOptions, result);
+  configureAcceleration(updatedOptions, result);
+
+  return result;
+}
+
+/**
+ * Configues the `externalFile` option and validates that a single model is
+ * provided.
+ */
+async function configureExternalFile(
+    options: BaseOptions, proto: BaseOptionsProto) {
+  const externalFile = proto.getModelAsset() || new ExternalFile();
+  proto.setModelAsset(externalFile);
+
+  if (options.modelAssetPath || options.modelAssetBuffer) {
+    if (options.modelAssetPath && options.modelAssetBuffer) {
+      throw new Error(
+          'Cannot set both baseOptions.modelAssetPath and baseOptions.modelAssetBuffer');
+    }
+
+    let modelAssetBuffer = options.modelAssetBuffer;
+    if (!modelAssetBuffer) {
+      const response = await fetch(options.modelAssetPath!.toString());
+      modelAssetBuffer = new Uint8Array(await response.arrayBuffer());
+    }
+    externalFile.setFileContent(modelAssetBuffer);
   }
-  if (!baseOptions.modelAssetPath && !baseOptions.modelAssetBuffer) {
+
+  if (!externalFile.hasFileContent()) {
     throw new Error(
         'Either baseOptions.modelAssetPath or baseOptions.modelAssetBuffer must be set');
   }
+}
 
-  let modelAssetBuffer = baseOptions.modelAssetBuffer;
-  if (!modelAssetBuffer) {
-    const response = await fetch(baseOptions.modelAssetPath!.toString());
-    modelAssetBuffer = new Uint8Array(await response.arrayBuffer());
+/** Configues the `acceleration` option. */
+function configureAcceleration(options: BaseOptions, proto: BaseOptionsProto) {
+  if ('delegate' in options) {
+    const acceleration = new Acceleration();
+    if (options.delegate === 'cpu') {
+      acceleration.setXnnpack(
+          new InferenceCalculatorOptions.Delegate.Xnnpack());
+      proto.setAcceleration(acceleration);
+    } else if (options.delegate === 'gpu') {
+      acceleration.setGpu(new InferenceCalculatorOptions.Delegate.Gpu());
+      proto.setAcceleration(acceleration);
+    } else {
+      proto.clearAcceleration();
+    }
   }
-
-  const proto = new BaseOptionsProto();
-  const externalFile = new ExternalFile();
-  externalFile.setFileContent(modelAssetBuffer);
-  proto.setModelAsset(externalFile);
-  return proto;
 }
