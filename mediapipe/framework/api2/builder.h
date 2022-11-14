c7d531ebb2b97a56a11f469cf59719f9892dbc9f
MediaPipe Team <mediapipe-team@google.com>
2022-11-14 10:38:38 -0800

    AddTarget -> ConnectTo and documentation
    PiperOrigin-RevId: 488407930

--- mediapipe/framework/api2/builder.h
+++ mediapipe/framework/api2/builder.h
@@ -176,21 +176,39 @@ class SourceImpl {
       : SourceImpl(&GetWithAutoGrow(vec, 0)) {}
   explicit SourceImpl(SourceBase* base) : base_(base) {}
 
+  // Connects MediaPipe stream or side packet to a destination:
+  // - node input (input stream) / side input (input side packet)
+  // - graph output (output stream) / side output (output side packet).
+  //
+  // MediaPipe streams and side packets can be connected to multiple
+  // destinations. Side packets and packets added to streams are sent to all
+  // connected destinations.
   template <typename U,
             typename std::enable_if<AllowConnection<U>{}, int>::type = 0>
-  Src& AddTarget(const Dst<U>& dest) {
+  Src& ConnectTo(const Dst<U>& dest) {
     CHECK(dest.base_.source == nullptr);
     dest.base_.source = base_;
     base_->dests_.emplace_back(&dest.base_);
     return *this;
   }
+
+  // Shortcut for `ConnectTo`.
+  //
+  // Connects MediaPipe stream or side packet to a destination:
+  // - node input (input stream) / side input (input side packet)
+  // - graph output (output stream) / side output (output side packet).
+  //
+  // MediaPipe streams and side packets can be connected to multiple
+  // destinations. Side packets and packets added to streams are sent to all
+  // connected destinations.
+  template <typename U>
+  Src& operator>>(const Dst<U>& dest) {
+    return ConnectTo(dest);
+  }
+
   Src& SetName(std::string name) {
     base_->name_ = std::move(name);
     return *this;
-  }
-  template <typename U>
-  Src& operator>>(const Dst<U>& dest) {
-    return AddTarget(dest);
   }
 
   template <typename U,
