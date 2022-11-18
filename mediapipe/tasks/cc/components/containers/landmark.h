03d388fecffe3734d8f6878f6f0def404065076b
MediaPipe Team <mediapipe-team@google.com>
2022-11-18 09:49:23 -0800

    Add hand landmark named index constants
    PiperOrigin-RevId: 489498248

---
+++ mediapipe/tasks/cc/components/containers/landmark.h
@@ -0,0 +1,48 @@
+/* Copyright 2022 The MediaPipe Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
+#ifndef MEDIAPIPE_TASKS_CC_COMPONENTS_CONTAINERS_LANDMARK_H_
+#define MEDIAPIPE_TASKS_CC_COMPONENTS_CONTAINERS_LANDMARK_H_
+
+namespace mediapipe::tasks::components::containers {
+
+// The 21 hand landmarks.
+enum HandLandmark {
+  WRIST = 0,
+  THUMB_CMC = 1,
+  THUMB_MCP = 2,
+  THUMB_IP = 3,
+  THUMB_TIP = 4,
+  INDEX_FINGER_MCP = 5,
+  INDEX_FINGER_PIP = 6,
+  INDEX_FINGER_DIP = 7,
+  INDEX_FINGER_TIP = 8,
+  MIDDLE_FINGER_MCP = 9,
+  MIDDLE_FINGER_PIP = 10,
+  MIDDLE_FINGER_DIP = 11,
+  MIDDLE_FINGER_TIP = 12,
+  RING_FINGER_MCP = 13,
+  RING_FINGER_PIP = 14,
+  RING_FINGER_DIP = 15,
+  RING_FINGER_TIP = 16,
+  PINKY_MCP = 17,
+  PINKY_PIP = 18,
+  PINKY_DIP = 19,
+  PINKY_TIP = 20
+};
+
+}  // namespace mediapipe::tasks::components::containers
+
+#endif  // MEDIAPIPE_TASKS_CC_COMPONENTS_CONTAINERS_LANDMARK_H_
--- mediapipe/tasks/python/components/containers/landmark.py
+++ mediapipe/tasks/python/components/containers/landmark.py
@@ -14,6 +14,7 @@
 """Landmark data class."""
 
 import dataclasses
+import enum
 from typing import Optional
 
 from mediapipe.framework.formats import landmark_pb2
@@ -120,3 +121,28 @@ class NormalizedLandmark:
         z=pb2_obj.z,
         visibility=pb2_obj.visibility,
         presence=pb2_obj.presence)
+
+
+class HandLandmark(enum.IntEnum):
+  """The 21 hand landmarks."""
+  WRIST = 0
+  THUMB_CMC = 1
+  THUMB_MCP = 2
+  THUMB_IP = 3
+  THUMB_TIP = 4
+  INDEX_FINGER_MCP = 5
+  INDEX_FINGER_PIP = 6
+  INDEX_FINGER_DIP = 7
+  INDEX_FINGER_TIP = 8
+  MIDDLE_FINGER_MCP = 9
+  MIDDLE_FINGER_PIP = 10
+  MIDDLE_FINGER_DIP = 11
+  MIDDLE_FINGER_TIP = 12
+  RING_FINGER_MCP = 13
+  RING_FINGER_PIP = 14
+  RING_FINGER_DIP = 15
+  RING_FINGER_TIP = 16
+  PINKY_MCP = 17
+  PINKY_PIP = 18
+  PINKY_DIP = 19
+  PINKY_TIP = 20
