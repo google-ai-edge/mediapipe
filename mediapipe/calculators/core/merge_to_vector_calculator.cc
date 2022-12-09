453d67de92d19abf2488a4400532708d734b20bb
Jiuqiang Tang <jqtang@google.com>
2022-12-09 13:10:25 -0800

    Add MergeDetectionsToVectorCalculator.
    PiperOrigin-RevId: 494246359

--- mediapipe/calculators/core/merge_to_vector_calculator.cc
+++ mediapipe/calculators/core/merge_to_vector_calculator.cc
@@ -15,6 +15,7 @@ limitations under the License.
 
 #include "mediapipe/calculators/core/merge_to_vector_calculator.h"
 
+#include "mediapipe/framework/formats/detection.pb.h"
 #include "mediapipe/framework/formats/image.h"
 
 namespace mediapipe {
@@ -26,6 +27,10 @@ MEDIAPIPE_REGISTER_NODE(MergeImagesToVectorCalculator);
 typedef MergeToVectorCalculator<mediapipe::GpuBuffer>
     MergeGpuBuffersToVectorCalculator;
 MEDIAPIPE_REGISTER_NODE(MergeGpuBuffersToVectorCalculator);
+
+typedef MergeToVectorCalculator<mediapipe::Detection>
+    MergeDetectionsToVectorCalculator;
+MEDIAPIPE_REGISTER_NODE(MergeDetectionsToVectorCalculator);
 
 }  // namespace api2
 }  // namespace mediapipe
