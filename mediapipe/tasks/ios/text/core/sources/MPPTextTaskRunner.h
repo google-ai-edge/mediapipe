0eacc333752c5b0ebe698748bac86a9fc91bf8a2
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2023-01-05 19:18:50 +0530

    Added MPPTextTaskRunner

---
+++ mediapipe/tasks/ios/text/core/sources/MPPTextTaskRunner.h
@@ -0,0 +1,37 @@
+// Copyright 2023 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import <Foundation/Foundation.h>
+#import "mediapipe/tasks/ios/core/sources/MPPTaskRunner.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ * This class is used to create and call appropriate methods on the C++ Task Runner to initialize, execute and terminate any Mediapipe text task. 
+ */
+@interface MPPTextTaskRunner : MPPTaskRunner
+
+/**
+ * Initializes a new `MPPTextTaskRunner` with the mediapipe task graph config proto.
+ *
+ * @param graphConfig A mediapipe task graph config proto.
+ *
+ * @return An instance of `MPPTextTaskRunner` initialized to the given graph config proto.
+ */
+- (instancetype)initWithCalculatorGraphConfig:(mediapipe::CalculatorGraphConfig)graphConfig
+                                        error:(NSError **)error NS_DESIGNATED_INITIALIZER;
+
+@end
+
+NS_ASSUME_NONNULL_END
