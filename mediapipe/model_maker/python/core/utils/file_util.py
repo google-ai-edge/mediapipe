d283e6a05abcba303884f1f7232c1ac64597554b
MediaPipe Team <mediapipe-team@google.com>
2023-01-31 18:41:42 -0800

    Support downloading model files on-demand from GCS in model_maker
    PiperOrigin-RevId: 506174708

--- mediapipe/model_maker/python/core/utils/file_util.py
+++ mediapipe/model_maker/python/core/utils/file_util.py
@@ -13,11 +13,93 @@
 # limitations under the License.
 """Utilities for files."""
 
+import dataclasses
 import os
+import pathlib
+import shutil
+import tarfile
+import tempfile
+import requests
 
 # resources dependency
 
 
+_TEMPDIR_FOLDER = 'model_maker'
+
+
+@dataclasses.dataclass
+class DownloadedFiles:
+  """File(s) that are downloaded from a url into a local directory.
+
+  If `is_folder` is True:
+    1. `path` should be a folder
+    2. `url` should point to a .tar.gz file which contains a single folder at
+      the root level.
+
+  Attributes:
+    path: Relative path in local directory.
+    url: GCS url to download the file(s).
+    is_folder: Whether the path and url represents a folder.
+  """
+
+  path: str
+  url: str
+  is_folder: bool = False
+
+  def get_path(self) -> str:
+    """Gets the path of files saved in a local directory.
+
+    If the path doesn't exist, this method will download the file(s) from the
+    provided url. The path is not cleaned up so it can be reused for subsequent
+    calls to the same path.
+    Folders are expected to be zipped in a .tar.gz file which will be extracted
+    into self.path in the local directory.
+
+    Raises:
+      RuntimeError: If the extracted folder does not have a singular root
+        directory.
+
+    Returns:
+      The absolute path to the downloaded file(s)
+    """
+    tmpdir = tempfile.gettempdir()
+    absolute_path = pathlib.Path(
+        os.path.join(tmpdir, _TEMPDIR_FOLDER, self.path)
+    )
+    if not absolute_path.exists():
+      print(f'Downloading {self.url} to {absolute_path}')
+      r = requests.get(self.url, allow_redirects=True)
+      if self.is_folder:
+        # Use tempf to store the downloaded .tar.gz file
+        tempf = tempfile.NamedTemporaryFile(suffix='.tar.gz', mode='wb')
+        tempf.write(r.content)
+        tarf = tarfile.open(tempf.name)
+        # Use tmpdir to store the extracted contents of the .tar.gz file
+        with tempfile.TemporaryDirectory() as tmpdir:
+          tarf.extractall(tmpdir)
+          tarf.close()
+          tempf.close()
+          subdirs = os.listdir(tmpdir)
+          # Make sure tmpdir only has one subdirectory
+          if len(subdirs) > 1 or not os.path.isdir(
+              os.path.join(tmpdir, subdirs[0])
+          ):
+            raise RuntimeError(
+                f"Extracted folder from {self.url} doesn't contain a "
+                f'single root directory: {subdirs}'
+            )
+          # Create the parent dir of absolute_path and copy the contents of the
+          # top level dir in the .tar.gz file into absolute_path.
+          pathlib.Path.mkdir(absolute_path.parent, parents=True, exist_ok=True)
+          shutil.copytree(os.path.join(tmpdir, subdirs[0]), absolute_path)
+      else:
+        pathlib.Path.mkdir(absolute_path.parent, parents=True, exist_ok=True)
+        with open(absolute_path, 'wb') as f:
+          f.write(r.content)
+    return str(absolute_path)
+
+
+# TODO Remove after text_classifier supports downloading on demand.
 def get_absolute_path(file_path: str) -> str:
   """Gets the absolute path of a file in the model_maker directory.
 
