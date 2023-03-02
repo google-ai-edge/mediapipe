0ba35cf1a714af06bbc171252e27f91fa85661d2
MediaPipe Team <mediapipe-team@google.com>
2023-03-02 12:26:42 -0800

    Internal change
    PiperOrigin-RevId: 513608516

--- mediapipe/calculators/image/image_cropping_calculator_test.cc
+++ mediapipe/calculators/image/image_cropping_calculator_test.cc
@@ -19,8 +19,12 @@
 
 #include "mediapipe/calculators/image/image_cropping_calculator.pb.h"
 #include "mediapipe/framework/calculator_framework.h"
+#include "mediapipe/framework/calculator_runner.h"
+#include "mediapipe/framework/formats/image_frame_opencv.h"
 #include "mediapipe/framework/formats/rect.pb.h"
 #include "mediapipe/framework/port/gtest.h"
+#include "mediapipe/framework/port/opencv_core_inc.h"
+#include "mediapipe/framework/port/opencv_imgproc_inc.h"
 #include "mediapipe/framework/port/parse_text_proto.h"
 #include "mediapipe/framework/port/status_matchers.h"
 #include "mediapipe/framework/tool/tag_map.h"
@@ -36,6 +40,166 @@ constexpr int input_height = 100;
 constexpr char kRectTag[] = "RECT";
 constexpr char kHeightTag[] = "HEIGHT";
 constexpr char kWidthTag[] = "WIDTH";
+
+std::unique_ptr<mediapipe::ImageFrame> GetInputFrame(int width, int height,
+                                                     int channel) {
+  const int total_size = width * height * channel;
+
+  auto image_format = channel == 4 ? mediapipe::ImageFormat::SRGBA
+                                   : mediapipe::ImageFormat::SRGB;
+
+  auto input_frame = std::make_unique<mediapipe::ImageFrame>(
+      image_format, width, height, /*alignment_boundary =*/1);
+  for (int i = 0; i < total_size; ++i) {
+    input_frame->MutablePixelData()[i] = i % 256;
+  }
+
+  return input_frame;
+}
+
+// Test identity function, where cropping size is same as input size
+TEST(ImageCroppingCalculatorTest, IdentityFunctionCropWithOriginalSize) {
+  auto calculator_node =
+      ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig::Node>(
+          absl::Substitute(
+              R"pb(
+                calculator: "ImageCroppingCalculator"
+                input_stream: "IMAGE:input_frames"
+                output_stream: "IMAGE:cropped_output_frames"
+                options: {
+                  [mediapipe.ImageCroppingCalculatorOptions.ext] {
+                    width: $0
+                    height: $1
+                  }
+                }
+              )pb",
+              input_width, input_height));
+  mediapipe::CalculatorRunner runner(calculator_node);
+
+  // Input frame.
+  const auto input_frame = GetInputFrame(input_width, input_height, 3);
+  auto input_frame_packet =
+      mediapipe::MakePacket<mediapipe::ImageFrame>(std::move(*input_frame));
+  runner.MutableInputs()->Tag("IMAGE").packets.push_back(
+      input_frame_packet.At(mediapipe::Timestamp(1)));
+
+  MP_ASSERT_OK(runner.Run());
+
+  const auto& outputs = runner.Outputs();
+  EXPECT_EQ(outputs.NumEntries(), 1);
+  const auto& output_image =
+      outputs.Tag("IMAGE").packets[0].Get<mediapipe::ImageFrame>();
+
+  const auto expected_output = GetInputFrame(input_width, input_height, 3);
+  cv::Mat output_mat = formats::MatView(&output_image);
+  cv::Mat expected_mat = formats::MatView(expected_output.get());
+  double max_diff = cv::norm(expected_mat, output_mat, cv::NORM_INF);
+  EXPECT_EQ(max_diff, 0);
+}  // TEST
+
+// Test identity function, where cropping size is same as input size.
+// When an image has an odd number for its size, its center falls on a
+// fractional pixel. As a result, the values for center_x and center_y need to
+// be of type float.
+TEST(ImageCroppingCalculatorTest, IdentityFunctionCropWithOddSize) {
+  const int input_width = 99;
+  const int input_height = 99;
+
+  auto calculator_node =
+      ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig::Node>(
+          absl::Substitute(
+              R"pb(
+                calculator: "ImageCroppingCalculator"
+                input_stream: "IMAGE:input_frames"
+                output_stream: "IMAGE:cropped_output_frames"
+                options: {
+                  [mediapipe.ImageCroppingCalculatorOptions.ext] {
+                    width: $0
+                    height: $1
+                  }
+                }
+              )pb",
+              input_width, input_height));
+  mediapipe::CalculatorRunner runner(calculator_node);
+
+  // Input frame.
+  const auto input_frame = GetInputFrame(input_width, input_height, 3);
+  auto input_frame_packet =
+      mediapipe::MakePacket<mediapipe::ImageFrame>(std::move(*input_frame));
+  runner.MutableInputs()->Tag("IMAGE").packets.push_back(
+      input_frame_packet.At(mediapipe::Timestamp(1)));
+
+  MP_ASSERT_OK(runner.Run());
+
+  const auto& outputs = runner.Outputs();
+  EXPECT_EQ(outputs.NumEntries(), 1);
+  const auto& output_image =
+      outputs.Tag("IMAGE").packets[0].Get<mediapipe::ImageFrame>();
+
+  const auto expected_output = GetInputFrame(input_width, input_height, 3);
+  cv::Mat output_mat = formats::MatView(&output_image);
+  cv::Mat expected_mat = formats::MatView(expected_output.get());
+  double max_diff = cv::norm(expected_mat, output_mat, cv::NORM_INF);
+  EXPECT_EQ(max_diff, 0);
+}  // TEST
+
+// Test identity function on GPU, where cropping size is same as input size.
+TEST(ImageCroppingCalculatorTest, IdentityFunctionCropWithOriginalSizeGPU) {
+  mediapipe::CalculatorGraphConfig config =
+      ParseTextProtoOrDie<mediapipe::CalculatorGraphConfig>(absl::Substitute(
+          R"pb(
+            input_stream: "input_frames"
+            node {
+              calculator: "ImageFrameToGpuBufferCalculator"
+              input_stream: "input_frames"
+              output_stream: "input_frames_gpu"
+            }
+            node {
+              calculator: "ImageCroppingCalculator"
+              input_stream: "IMAGE_GPU:input_frames_gpu"
+              output_stream: "IMAGE_GPU:cropped_output_frames_gpu"
+              options: {
+                [mediapipe.ImageCroppingCalculatorOptions.ext] {
+                  width: $0
+                  height: $1
+                }
+              }
+            }
+            node {
+              calculator: "GpuBufferToImageFrameCalculator"
+              input_stream: "cropped_output_frames_gpu"
+              output_stream: "cropped_output_frames"
+            }
+          )pb",
+          input_width, input_height));
+
+  std::vector<Packet> output_packets;
+  tool::AddVectorSink("cropped_output_frames", &config, &output_packets);
+
+  CalculatorGraph graph;
+  MP_ASSERT_OK(graph.Initialize(config));
+
+  // Input frame.
+  const auto input_frame = GetInputFrame(input_width, input_height, 4);
+  auto input_frame_packet =
+      mediapipe::MakePacket<mediapipe::ImageFrame>(std::move(*input_frame));
+
+  MP_ASSERT_OK(graph.StartRun({}));
+  MP_ASSERT_OK(graph.AddPacketToInputStream(
+      "input_frames", input_frame_packet.At(mediapipe::Timestamp(1))));
+  MP_ASSERT_OK(graph.WaitUntilIdle());
+
+  // Get and process results.
+  const ImageFrame& output_image = output_packets[0].Get<ImageFrame>();
+  std::cout << output_image.Width();
+
+  const auto expected_output = GetInputFrame(input_width, input_height, 4);
+  cv::Mat output_mat = formats::MatView(&output_image);
+  cv::Mat expected_mat = formats::MatView(expected_output.get());
+  double max_diff = cv::norm(expected_mat, output_mat, cv::NORM_INF);
+
+  EXPECT_EQ(max_diff, 0);
+}  // TEST
 
 // Test normal case, where norm_width and norm_height in options are set.
 TEST(ImageCroppingCalculatorTest, GetCroppingDimensionsNormal) {
