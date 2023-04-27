7b055df21193647540b934d5a25fdcbac805d477
MediaPipe Team <mediapipe-team@google.com>
2023-04-26 22:11:06 -0700

    Internal change
    PiperOrigin-RevId: 527473249

--- mediapipe/framework/tool/tag_map_test.cc
+++ mediapipe/framework/tool/tag_map_test.cc
@@ -101,7 +101,7 @@ void TestSuccessTagMap(const std::vector<std::string>& tag_index_names,
   EXPECT_EQ(tags.size(), tag_map->Mapping().size())
       << "Parameters: in " << tag_map->DebugString();
   for (int i = 0; i < tags.size(); ++i) {
-    EXPECT_TRUE(mediapipe::ContainsKey(tag_map->Mapping(), tags[i]))
+    EXPECT_TRUE(tag_map->Mapping().contains(tags[i]))
         << "Parameters: Trying to find \"" << tags[i] << "\" in\n"
         << tag_map->DebugString();
   }
