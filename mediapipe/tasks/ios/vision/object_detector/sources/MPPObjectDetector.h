c58cb97055f06ae0d359d73ce6bc4615cc356401
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2023-05-02 07:47:49 +0530

    Updated documentatio

--- mediapipe/tasks/ios/vision/object_detector/sources/MPPObjectDetector.h
+++ mediapipe/tasks/ios/vision/object_detector/sources/MPPObjectDetector.h
@@ -137,6 +137,9 @@ NS_SWIFT_NAME(ObjectDetector)
  * the provided `MPPImage`. Only use this method when the `MPPObjectDetector` is created with
  * `MPPRunningModeLiveStream`. Results are provided asynchronously via the `completion` callback
  * provided in the `MPPObjectDetectorOptions`.
+ * The object which needs to be continuously notified of the available results of object 
+ * detection must confirm to `MPPObjectDetectorDelegate` protocol and implement the 
+ * `objectDetector:didFinishDetectionWithResult:timestampInMilliseconds:error:` delegate method.
  *
  * It's required to provide a timestamp (in milliseconds) to indicate when the input image is sent
  * to the object detector. The input timestamps must be monotonically increasing.
