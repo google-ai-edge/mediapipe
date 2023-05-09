f77481f303c91e08a8cb5b7ecf81b7f69ddf5b7b
MediaPipe Team <mediapipe-team@google.com>
2023-05-09 15:55:20 -0700

    MediaPipe GPU: Log renderer.
    We currently log GL version, but since we support multiple backends, logging the renderer as well takes away any doubt what is being used at runtime.
    
    PiperOrigin-RevId: 530736209

--- mediapipe/gpu/gl_context.cc
+++ mediapipe/gpu/gl_context.cc
@@ -340,7 +340,9 @@ absl::Status GlContext::FinishInitialization(bool create_thread) {
     }
 
     LOG(INFO) << "GL version: " << gl_major_version_ << "." << gl_minor_version_
-              << " (" << version_string << ")";
+              << " (" << version_string
+              << "), renderer: " << glGetString(GL_RENDERER);
+
     {
       auto status = GetGlExtensions();
       if (!status.ok()) {
