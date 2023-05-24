1aa44abcaba07ab2e5ba768d6f3e9882a3e694f9
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2023-05-24 20:25:45 +0530

    Revert "Added support to set delegates in MPPBaseOptions"
    This reverts commit 1e1693d9aaf348fdf1d2fa9d2836bd76056b1c54.

--- mediapipe/tasks/ios/core/sources/MPPBaseOptions.h
+++ mediapipe/tasks/ios/core/sources/MPPBaseOptions.h
@@ -17,33 +17,14 @@
 NS_ASSUME_NONNULL_BEGIN
 
 /**
- * MediaPipe Tasks delegate.
- */
- typedef NS_ENUM(NSUInteger, MPPDelegate) {
-   /** CPU. */
-   MPPDelegateCPU,
-
-   /** GPU. */
-   MPPDelegateGPU
- } NS_SWIFT_NAME(Delegate);
-
-/**
  * Holds the base options that is used for creation of any type of task. It has fields with
  * important information acceleration configuration, TFLite model source etc.
  */
 NS_SWIFT_NAME(BaseOptions)
 @interface MPPBaseOptions : NSObject <NSCopying>
 
-/**
- * The absolute path to a model asset file (a tflite model or a model asset bundle file) stored in the app bundle.
- */
+/** The path to the model asset to open and mmap in memory. */
 @property(nonatomic, copy) NSString *modelAssetPath;
-
-/**
- * Device delegate to run the MediaPipe pipeline. If the delegate is not set, the default
- * delegate CPU is used.
- */
-@property(nonatomic) MPPDelegate delegate;
 
 @end
 
