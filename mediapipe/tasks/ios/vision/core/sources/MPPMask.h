2cdb291e544905d85a5a50c085316dd46a53a58e
Prianka Liz Kariat <prianka.kariat@ymedialabs.com>
2023-06-13 22:29:15 +0530

    Removed core video import

--- mediapipe/tasks/ios/vision/core/sources/MPPMask.h
+++ mediapipe/tasks/ios/vision/core/sources/MPPMask.h
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#import <CoreVideo/CoreVideo.h>
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
