f72542ae5db8967b935c3c64ff0577a434ba2ef4
MediaPipe Team <mediapipe-team@google.com>
2023-10-09 21:07:37 -0700

    Internal change
    PiperOrigin-RevId: 572122579

--- mediapipe/tasks/cc/vision/pose_landmarker/pose_topology.h
+++ mediapipe/tasks/cc/vision/pose_landmarker/pose_topology.h
@@ -1,11 +1,8 @@
 /* Copyright 2023 The MediaPipe Authors.
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -13,18 +10,15 @@ See the License for the specific language governing permissions and
 limitations under the License.
 ==============================================================================*/
 
-#ifndef MEDIAPIPE_TASKS_CC_VISION_POSE_LANDMARKER_POSE_LANDMARK_H_
-#define MEDIAPIPE_TASKS_CC_VISION_POSE_LANDMARKER_POSE_LANDMARK_H_
+#ifndef MEDIAPIPE_TASKS_CC_VISION_POSE_LANDMARKER_POSE_TOPOLOGY_H_
+#define MEDIAPIPE_TASKS_CC_VISION_POSE_LANDMARKER_POSE_TOPOLOGY_H_
 
-namespace mediapipe {
-namespace tasks {
-namespace vision {
-namespace pose_landmarker {
+namespace mediapipe::tasks::vision::pose_landmarker {
 
 static constexpr int kNumPoseLandmarks = 33;
 
 // BlazePose 33 landmark names.
-enum class PoseLandmark {
+enum class PoseLandmarkName {
   kNose = 0,
   kLeftEyeInner,
   kLeftEye,
@@ -60,9 +54,6 @@ enum class PoseLandmark {
   kRightFootIndex,
 };
 
-}  // namespace pose_landmarker
-}  // namespace vision
-}  // namespace tasks
-}  // namespace mediapipe
+}  // namespace mediapipe::tasks::vision::pose_landmarker
 
-#endif  // MEDIAPIPE_TASKS_CC_VISION_POSE_LANDMARKER_POSE_LANDMARK_H_
+#endif  // MEDIAPIPE_TASKS_CC_VISION_POSE_LANDMARKER_POSE_TOPOLOGY_H_
