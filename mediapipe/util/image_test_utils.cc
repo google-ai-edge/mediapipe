f13c6974ee046791d6b5a4af4e77ab81ea8588c0
MediaPipe Team <mediapipe-team@google.com>
2023-11-16 02:31:58 -0800

    Extract CPU conversion methods into a separate library & add test
    PiperOrigin-RevId: 582966041

--- mediapipe/util/image_test_utils.cc
+++ mediapipe/util/image_test_utils.cc
@@ -17,6 +17,34 @@
 
 namespace mediapipe {
 
+namespace {
+
+template <ImageFormat::Format Format, typename DataType>
+ImageFrame CreateTestImageFrame(int width, int height, DataType max_value) {
+  ImageFrame image_frame(Format, width, height,
+                         /*alignment_boundary=*/1);
+  const int num_channels = image_frame.NumberOfChannels();
+  const float num_values = width * height * num_channels;
+  uint8_t* const data_ptr =
+      reinterpret_cast<uint8_t*>(image_frame.MutablePixelData());
+  for (int y = 0; y < height; ++y) {
+    uint8_t* const row = data_ptr + image_frame.WidthStep() * y;
+    for (int x = 0; x < width; ++x) {
+      DataType* pixel = reinterpret_cast<DataType*>(row) + x * num_channels;
+      for (int c = 0; c < num_channels; ++c) {
+        // Fill pixel channel with a value in [0:max_value] range.
+        pixel[c] =
+            static_cast<DataType>(static_cast<float>(y * width * num_channels +
+                                                     x * num_channels + c) /
+                                  num_values * max_value);
+      }
+    }
+  }
+  return image_frame;
+}
+
+}  // namespace
+
 cv::Mat GetRgb(const std::string& path) {
   cv::Mat bgr = cv::imread(path);
   cv::Mat rgb;
@@ -69,6 +97,16 @@ cv::Mat RgbaToBgr(cv::Mat rgba) {
   cv::Mat bgra;
   cv::cvtColor(rgba, bgra, cv::COLOR_RGBA2BGR);
   return bgra;
+}
+
+ImageFrame CreateTestFloat32ImageFrame(int width, int height) {
+  return CreateTestImageFrame<ImageFormat::VEC32F1, float>(width, height,
+                                                           /*max_value=*/1.0f);
+}
+
+ImageFrame CreateTestGrey8ImageFrame(int width, int height) {
+  return CreateTestImageFrame<ImageFormat::GRAY8, uint8_t>(width, height,
+                                                           /*max_value=*/255);
 }
 
 }  // namespace mediapipe
