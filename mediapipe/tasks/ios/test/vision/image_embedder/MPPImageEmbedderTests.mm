553c3fd95c61f111fae42b5c89b037938bd6f576
Prianka Liz Kariat <prianka.kariat@codeandtheory.com>
2024-01-09 18:54:15 +0530

    Added running mode tests to iOS Objective C Image Embedder Tests

--- mediapipe/tasks/ios/test/vision/image_embedder/MPPImageEmbedderTests.mm
+++ mediapipe/tasks/ios/test/vision/image_embedder/MPPImageEmbedderTests.mm
@@ -35,6 +35,9 @@ static MPPFileInfo *const kMobileNetEmbedderModelFileInfo =
 
 static NSString *const kExpectedErrorDomain = @"com.google.mediapipe.tasks";
 
+static NSString *const kLiveStreamTestsDictImageEmbedderKey = @"image_embedder";
+static NSString *const kLiveStreamTestsDictExpectationKey = @"expectation";
+
 constexpr double kDoubleDifferenceTolerance = 1e-4;
 constexpr NSInteger kExpectedEmbeddingLength = 1024;
 
@@ -47,7 +50,7 @@ constexpr NSInteger kExpectedEmbeddingLength = 1024;
 #define AssertImageEmbedderResultHasOneEmbedding(imageEmbedderResult) \
   XCTAssertNotNil(imageEmbedderResult);                               \
   XCTAssertNotNil(imageEmbedderResult.embeddingResult);               \
-  \
+                                                                      \
   XCTAssertEqual(imageEmbedderResult.embeddingResult.embeddings.count, 1);
 
 #define AssertEmbeddingHasCorrectTypeAndDimension(embedding, quantized, expectedLength) \
@@ -61,7 +64,10 @@ constexpr NSInteger kExpectedEmbeddingLength = 1024;
     XCTAssertEqual(embedding.floatEmbedding.count, expectedLength);                     \
   }
 
-@interface MPPImageEmbedderTests : XCTestCase
+@interface MPPImageEmbedderTests : XCTestCase <MPPImageEmbedderLiveStreamDelegate> {
+  NSDictionary<NSString *, id> *_liveStreamSucceedsTestDict;
+  NSDictionary<NSString *, id> *_outOfOrderTimestampTestDict;
+}
 @end
 
 @implementation MPPImageEmbedderTests
@@ -101,7 +107,7 @@ constexpr NSInteger kExpectedEmbeddingLength = 1024;
 
 - (void)testEmbedWithQuantizationSucceeds {
   MPPImageEmbedderOptions *options =
-      [self imageSegmenterOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
   options.quantize = YES;
 
   MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
@@ -116,7 +122,7 @@ constexpr NSInteger kExpectedEmbeddingLength = 1024;
 
 - (void)testEmbedWithL2NormalizationSucceeds {
   MPPImageEmbedderOptions *options =
-      [self imageSegmenterOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
   options.l2Normalize = YES;
 
   MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
@@ -169,9 +175,279 @@ constexpr NSInteger kExpectedEmbeddingLength = 1024;
                             andCosineSimilarity:0.98086f];
 }
 
+- (void)testEmbedWithRegionOfInterestOrientationSucceeds {
+  MPPImageEmbedder *imageEmbedder =
+      [[MPPImageEmbedder alloc] initWithModelPath:kMobileNetEmbedderModelFileInfo.path error:nil];
+
+  MPPImage *burgerRotatedImage = [self assertCreateImageWithFileInfo:kBurgerRotatedImageFileInfo
+                                                         orientation:UIImageOrientationLeft];
+  MPPImageEmbedderResult *burgerRotatedImageResult =
+      [imageEmbedder embedImage:burgerRotatedImage
+               regionOfInterest:CGRectMake(0.0f, 0.0f, 1.0f, 0.833333f)
+                          error:nil];
+
+  MPPImage *burgerCroppedImage = [self assertCreateImageWithFileInfo:kBurgerCroppedImageFileInfo];
+  MPPImageEmbedderResult *burgerCroppedImageResult = [imageEmbedder embedImage:burgerCroppedImage
+                                                                         error:nil];
+
+  [self assertImageEmbedderResult:burgerRotatedImageResult
+      isApproximatelyEqualToImageEmbedderResult:burgerCroppedImageResult
+                                    isQuantized:NO
+                             withExpectedLength:kExpectedEmbeddingLength
+                            andCosineSimilarity:0.97749f];
+}
+
+#pragma mark Running Mode Tests
+
+- (void)testCreateImageEmbedderFailsWithMissingDelegateInLiveStreamMode {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+
+  options.runningMode = MPPRunningModeLiveStream;
+
+  [self assertCreateImageEmbedderWithOptions:options
+                      failsWithExpectedError:
+                          [NSError errorWithDomain:kExpectedErrorDomain
+                                              code:MPPTasksErrorCodeInvalidArgumentError
+                                          userInfo:@{
+                                            NSLocalizedDescriptionKey :
+                                                @"The vision task is in live stream mode. An "
+                                                @"object must be set as the delegate of the task "
+                                                @"in its options to ensure asynchronous delivery "
+                                                @"of results."
+                                          }]];
+}
+
+- (void)testEmbedFailsWithCallingWrongApiInImageMode {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+
+  MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
+
+  MPPImage *image = [self assertCreateImageWithFileInfo:kBurgerImageFileInfo];
+
+  NSError *liveStreamApiCallError;
+  XCTAssertFalse([imageEmbedder embedAsyncImage:image
+                        timestampInMilliseconds:0
+                                          error:&liveStreamApiCallError]);
+
+  NSError *expectedLiveStreamApiCallError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey : @"The vision task is not initialized with live "
+                                                    @"stream mode. Current Running Mode: Image"
+                      }];
+
+  AssertEqualErrors(liveStreamApiCallError, expectedLiveStreamApiCallError);
+
+  NSError *videoApiCallError;
+  XCTAssertFalse([imageEmbedder embedVideoFrame:image
+                        timestampInMilliseconds:0
+                                          error:&videoApiCallError]);
+
+  NSError *expectedVideoApiCallError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey : @"The vision task is not initialized with "
+                                                    @"video mode. Current Running Mode: Image"
+                      }];
+  AssertEqualErrors(videoApiCallError, expectedVideoApiCallError);
+}
+
+- (void)testEmbedFailsWithCallingWrongApiInVideoMode {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+  options.runningMode = MPPRunningModeVideo;
+
+  MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
+
+  MPPImage *image = [self assertCreateImageWithFileInfo:kBurgerImageFileInfo];
+
+  NSError *liveStreamApiCallError;
+  XCTAssertFalse([imageEmbedder embedAsyncImage:image
+                        timestampInMilliseconds:0
+                                          error:&liveStreamApiCallError]);
+
+  NSError *expectedLiveStreamApiCallError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey : @"The vision task is not initialized with live "
+                                                    @"stream mode. Current Running Mode: Video"
+                      }];
+
+  AssertEqualErrors(liveStreamApiCallError, expectedLiveStreamApiCallError);
+
+  NSError *imageApiCallError;
+  XCTAssertFalse([imageEmbedder embedImage:image error:&imageApiCallError]);
+
+  NSError *expectedImageApiCallError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey : @"The vision task is not initialized with "
+                                                    @"image mode. Current Running Mode: Video"
+                      }];
+  AssertEqualErrors(imageApiCallError, expectedImageApiCallError);
+}
+
+- (void)testEmbedFailsWithCallingWrongApiInLiveStreamMode {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+  options.runningMode = MPPRunningModeLiveStream;
+  options.imageEmbedderLiveStreamDelegate = self;
+
+  MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
+
+  MPPImage *image = [self assertCreateImageWithFileInfo:kBurgerImageFileInfo];
+
+  NSError *imageApiCallError;
+  XCTAssertFalse([imageEmbedder embedImage:image error:&imageApiCallError]);
+
+  NSError *expectedImageApiCallError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey : @"The vision task is not initialized with "
+                                                    @"image mode. Current Running Mode: Live Stream"
+                      }];
+  AssertEqualErrors(imageApiCallError, expectedImageApiCallError);
+
+  NSError *videoApiCallError;
+  XCTAssertFalse([imageEmbedder embedVideoFrame:image
+                        timestampInMilliseconds:0
+                                          error:&videoApiCallError]);
+
+  NSError *expectedVideoApiCallError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey : @"The vision task is not initialized with "
+                                                    @"video mode. Current Running Mode: Live Stream"
+                      }];
+  AssertEqualErrors(videoApiCallError, expectedVideoApiCallError);
+}
+
+- (void)testEmbedWithVideoModeSucceeds {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+  options.runningMode = MPPRunningModeVideo;
+
+  MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
+
+  MPPImage *image = [self assertCreateImageWithFileInfo:kBurgerImageFileInfo];
+
+  for (int i = 0; i < 3; i++) {
+    MPPImageEmbedderResult *imageEmbedderResult = [imageEmbedder embedVideoFrame:image
+                                                         timestampInMilliseconds:i
+                                                                           error:nil];
+
+    AssertImageEmbedderResultHasOneEmbedding(imageEmbedderResult);
+    AssertEmbeddingHasCorrectTypeAndDimension(imageEmbedderResult.embeddingResult.embeddings[0],
+                                              options.quantize, kExpectedEmbeddingLength);
+  }
+}
+
+- (void)testEmbedWithOutOfOrderTimestampsAndLiveStreamModeFails {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+  options.runningMode = MPPRunningModeLiveStream;
+  options.imageEmbedderLiveStreamDelegate = self;
+
+  XCTestExpectation *expectation = [[XCTestExpectation alloc]
+      initWithDescription:@"embedWiththOutOfOrderTimestampsAndLiveStream"];
+
+  expectation.expectedFulfillmentCount = 1;
+
+  MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
+
+  _outOfOrderTimestampTestDict = @{
+    kLiveStreamTestsDictImageEmbedderKey : imageEmbedder,
+    kLiveStreamTestsDictExpectationKey : expectation
+  };
+
+  MPPImage *image = [self assertCreateImageWithFileInfo:kBurgerImageFileInfo];
+
+  XCTAssertTrue([imageEmbedder embedAsyncImage:image timestampInMilliseconds:1 error:nil]);
+
+  NSError *error;
+  XCTAssertFalse([imageEmbedder embedAsyncImage:image timestampInMilliseconds:0 error:&error]);
+
+  NSError *expectedError =
+      [NSError errorWithDomain:kExpectedErrorDomain
+                          code:MPPTasksErrorCodeInvalidArgumentError
+                      userInfo:@{
+                        NSLocalizedDescriptionKey :
+                            @"INVALID_ARGUMENT: Input timestamp must be monotonically increasing."
+                      }];
+  AssertEqualErrors(error, expectedError);
+
+  NSTimeInterval timeout = 0.5f;
+  [self waitForExpectations:@[ expectation ] timeout:timeout];
+}
+
+- (void)testDetectWithLiveStreamModeSucceeds {
+  MPPImageEmbedderOptions *options =
+      [self imageEmbedderOptionsWithModelFileInfo:kMobileNetEmbedderModelFileInfo];
+  options.runningMode = MPPRunningModeLiveStream;
+  options.imageEmbedderLiveStreamDelegate = self;
+
+  NSInteger iterationCount = 100;
+
+  // Because of flow limiting, we cannot ensure that the callback will be invoked `iterationCount`
+  // times. An normal expectation will fail if expectation.fulfill() is not called
+  // `expectation.expectedFulfillmentCount` times. If `expectation.isInverted = true`, the test will
+  // only succeed if expectation is not fulfilled for the specified `expectedFulfillmentCount`.
+  // Since in our case we cannot predict how many times the expectation is supposed to be fulfilled
+  // setting, `expectation.expectedFulfillmentCount` = `iterationCount` + 1 and
+  // `expectation.isInverted = true` ensures that test succeeds if the expectation is fulfilled <=
+  // `iterationCount` times.
+  XCTestExpectation *expectation =
+      [[XCTestExpectation alloc] initWithDescription:@"embedWithLiveStream"];
+
+  expectation.expectedFulfillmentCount = iterationCount + 1;
+  expectation.inverted = YES;
+
+  MPPImageEmbedder *imageEmbedder = [self createImageEmbedderWithOptionsSucceeds:options];
+
+  _liveStreamSucceedsTestDict = @{
+    kLiveStreamTestsDictImageEmbedderKey : imageEmbedder,
+    kLiveStreamTestsDictExpectationKey : expectation
+  };
+
+  // TODO: Mimic initialization from CMSampleBuffer as live stream mode is most likely to be used
+  // with the iOS camera. AVCaptureVideoDataOutput sample buffer delegates provide frames of type
+  // `CMSampleBuffer`.
+  MPPImage *image = [self assertCreateImageWithFileInfo:kBurgerImageFileInfo];
+
+  for (int i = 0; i < iterationCount; i++) {
+    XCTAssertTrue([imageEmbedder embedAsyncImage:image timestampInMilliseconds:i error:nil]);
+  }
+
+  NSTimeInterval timeout = 0.5f;
+  [self waitForExpectations:@[ expectation ] timeout:timeout];
+}
+
+- (void)imageEmbedder:(MPPImageEmbedder *)imageEmbedder
+    didFinishEmbeddingWithResult:(MPPImageEmbedderResult *)imageEmbedderResult
+         timestampInMilliseconds:(NSInteger)timestampInMilliseconds
+                           error:(NSError *)error {
+  AssertImageEmbedderResultHasOneEmbedding(imageEmbedderResult);
+  AssertEmbeddingHasCorrectTypeAndDimension(imageEmbedderResult.embeddingResult.embeddings[0], NO,
+                                            kExpectedEmbeddingLength);
+
+  if (imageEmbedder == _outOfOrderTimestampTestDict[kLiveStreamTestsDictImageEmbedderKey]) {
+    [_outOfOrderTimestampTestDict[kLiveStreamTestsDictExpectationKey] fulfill];
+  } else if (imageEmbedder == _liveStreamSucceedsTestDict[kLiveStreamTestsDictImageEmbedderKey]) {
+    [_liveStreamSucceedsTestDict[kLiveStreamTestsDictExpectationKey] fulfill];
+  }
+}
+
 #pragma mark - Image Embedder Initializers
 
-- (MPPImageEmbedderOptions *)imageSegmenterOptionsWithModelFileInfo:(MPPFileInfo *)fileInfo {
+- (MPPImageEmbedderOptions *)imageEmbedderOptionsWithModelFileInfo:(MPPFileInfo *)fileInfo {
   MPPImageEmbedderOptions *options = [[MPPImageEmbedderOptions alloc] init];
   options.baseOptions.modelAssetPath = fileInfo.path;
   return options;
