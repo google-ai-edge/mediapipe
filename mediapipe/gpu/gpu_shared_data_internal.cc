f9f3851c6c6d3fad0d3c89017d7e7d0c753c10cb
MediaPipe Team <mediapipe-team@google.com>
2024-05-15 12:12:36 -0700

    Enables the configuration of GpuBufferPool options via GpuResources::Create();
    PiperOrigin-RevId: 634035115

--- mediapipe/gpu/gpu_shared_data_internal.cc
+++ mediapipe/gpu/gpu_shared_data_internal.cc
@@ -26,6 +26,7 @@
 #include "mediapipe/gpu/gl_context.h"
 #include "mediapipe/gpu/gl_context_options.pb.h"
 #include "mediapipe/gpu/graph_support.h"
+#include "mediapipe/gpu/multi_pool.h"
 
 #if __APPLE__
 #include "mediapipe/gpu/metal_shared_resources.h"
@@ -59,7 +60,7 @@ static constexpr bool kGlCalculatorShareContext = true;
 class GlContextExecutor : public Executor {
  public:
   explicit GlContextExecutor(GlContext* gl_context) : gl_context_(gl_context) {}
-  ~GlContextExecutor() override {}
+  ~GlContextExecutor() override = default;
   void Schedule(std::function<void()> task) override {
     gl_context_->RunWithoutWaiting(std::move(task));
   }
@@ -78,16 +79,18 @@ GpuResources::StatusOrGpuResources GpuResources::Create() {
 }
 
 GpuResources::StatusOrGpuResources GpuResources::Create(
-    PlatformGlContext external_context) {
+    PlatformGlContext external_context,
+    const MultiPoolOptions* gpu_buffer_pool_options) {
   MP_ASSIGN_OR_RETURN(
       std::shared_ptr<GlContext> context,
       GlContext::Create(external_context, kGlContextUseDedicatedThread));
   std::shared_ptr<GpuResources> gpu_resources(
-      new GpuResources(std::move(context)));
+      new GpuResources(std::move(context), gpu_buffer_pool_options));
   return gpu_resources;
 }
 
-GpuResources::GpuResources(std::shared_ptr<GlContext> gl_context)
+GpuResources::GpuResources(std::shared_ptr<GlContext> gl_context,
+                           const MultiPoolOptions* gpu_buffer_pool_options)
     : gl_key_context_(new GlContextMapType(),
                       [](auto* map) {
                         // This flushes all pending jobs in all GL contexts,
@@ -103,15 +106,19 @@ GpuResources::GpuResources(std::shared_ptr<GlContext> gl_context)
                               << "Failed to flush GlContext jobs: " << status;
                         }
                         delete map;
-                      })
+                      }),
 #if MEDIAPIPE_GPU_BUFFER_USE_CV_PIXEL_BUFFER
-      ,
       texture_caches_(std::make_shared<CvTextureCacheManager>()),
       gpu_buffer_pool_(
           [tc = texture_caches_](const internal::GpuBufferSpec& spec,
                                  const MultiPoolOptions& options) {
             return CvPixelBufferPoolWrapper::Create(spec, options, tc.get());
-          })
+          },
+          gpu_buffer_pool_options ? *gpu_buffer_pool_options
+                                  : kDefaultMultiPoolOptions)
+#else   // MEDIAPIPE_GPU_BUFFER_USE_CV_PIXEL_BUFFER
+          gpu_buffer_pool_(gpu_buffer_pool_options ? *gpu_buffer_pool_options
+                                            : kDefaultMultiPoolOptions)
 #endif  // MEDIAPIPE_GPU_BUFFER_USE_CV_PIXEL_BUFFER
 {
   gl_key_context_->insert({SharedContextKey(), gl_context});
--- mediapipe/gpu/gpu_shared_data_internal.h
+++ mediapipe/gpu/gpu_shared_data_internal.h
@@ -23,6 +23,7 @@
 
 #include <memory>
 
+#include "absl/status/statusor.h"
 #include "mediapipe/framework/calculator_context.h"
 #include "mediapipe/framework/calculator_node.h"
 #include "mediapipe/framework/executor.h"
@@ -30,6 +31,7 @@
 #include "mediapipe/gpu/gl_base.h"
 #include "mediapipe/gpu/gl_context.h"
 #include "mediapipe/gpu/gpu_buffer_multi_pool.h"
+#include "mediapipe/gpu/multi_pool.h"
 
 #ifdef __APPLE__
 #include "mediapipe/gpu/cv_texture_cache_manager.h"
@@ -47,7 +49,11 @@ class GpuResources {
   using StatusOrGpuResources = absl::StatusOr<std::shared_ptr<GpuResources>>;
 
   static StatusOrGpuResources Create();
-  static StatusOrGpuResources Create(PlatformGlContext external_context);
+  // Optional gpu_buffer_pool_options argument allows to configure the
+  // GpuBufferMultiPool instance.
+  static StatusOrGpuResources Create(
+      PlatformGlContext external_context,
+      const MultiPoolOptions* gpu_buffer_pool_options = nullptr);
 
   // The destructor must be defined in the implementation file so that on iOS
   // the correct ARC release calls are generated.
@@ -78,7 +84,8 @@ class GpuResources {
 
  private:
   GpuResources() = delete;
-  explicit GpuResources(std::shared_ptr<GlContext> gl_context);
+  explicit GpuResources(std::shared_ptr<GlContext> gl_context,
+                        const MultiPoolOptions* gpu_buffer_pool_options);
 
   GlContext::StatusOrGlContext GetOrCreateGlContext(const std::string& key);
   const std::string& ContextKey(const std::string& canonical_node_name);
