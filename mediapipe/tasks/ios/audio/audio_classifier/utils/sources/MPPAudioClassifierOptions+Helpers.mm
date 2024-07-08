611b1a5e00aeb5b566ef6ddc965e7dce5ce0a85b
Prianka Liz Kariat <prianka.kariat@codeandtheory.com>
2024-07-08 20:56:28 +0530

    Fixed incorrect stream mode in iOS audio classifier options

--- mediapipe/tasks/ios/audio/audio_classifier/utils/sources/MPPAudioClassifierOptions+Helpers.mm
+++ mediapipe/tasks/ios/audio/audio_classifier/utils/sources/MPPAudioClassifierOptions+Helpers.mm
@@ -34,7 +34,7 @@ using ClassifierOptionsProto = ::mediapipe::tasks::components::processors::proto
       optionsProto->MutableExtension(AudioClassifierGraphOptionsProto::ext);
 
   [self.baseOptions copyToProto:graphOptions->mutable_base_options()
-              withUseStreamMode:self.runningMode != MPPAudioRunningModeAudioStream];
+              withUseStreamMode:self.runningMode != MPPAudioRunningModeAudioClips];
 
   ClassifierOptionsProto *classifierOptionsProto = graphOptions->mutable_classifier_options();
   classifierOptionsProto->Clear();
