a11067718c485cdcca4efa1c8ea2b4c88d660ef4
MediaPipe Team <mediapipe-team@google.com>
2024-07-19 09:36:16 -0700

    Add option to use shifted window in SpectrogramCalculator. This is useful create window non-zero endpoints by shifting a half sample.
    PiperOrigin-RevId: 654032482

--- mediapipe/calculators/audio/spectrogram_calculator_test.cc
+++ mediapipe/calculators/audio/spectrogram_calculator_test.cc
@@ -18,13 +18,13 @@
 #include <complex>
 #include <cstdint>
 #include <memory>
-#include <numeric>
 #include <optional>
 #include <string>
 #include <vector>
 
 #include "Eigen/Core"
 #include "absl/log/absl_log.h"
+#include "absl/status/status.h"
 #include "audio/dsp/number_util.h"
 #include "mediapipe/calculators/audio/spectrogram_calculator.pb.h"
 #include "mediapipe/framework/calculator_framework.h"
@@ -34,8 +34,8 @@
 #include "mediapipe/framework/port/benchmark.h"
 #include "mediapipe/framework/port/gmock.h"
 #include "mediapipe/framework/port/gtest.h"
-#include "mediapipe/framework/port/status.h"
 #include "mediapipe/util/time_series_test_util.h"
+#include "mediapipe/util/time_series_util.h"
 
 namespace mediapipe {
 namespace {
@@ -953,6 +953,35 @@ TEST_F(SpectrogramCalculatorTest,
     EXPECT_EQ(spectrogram_num_rows, spectrograms[i].rows());
     EXPECT_EQ(spectrogram_num_cols, spectrograms[i].cols());
   }
+}
+
+TEST_F(SpectrogramCalculatorTest, ShiftedWindowWorks) {
+  options_.set_frame_duration_seconds(100.0 / input_sample_rate_);
+  options_.set_frame_overlap_seconds(0.0);
+  options_.set_use_shifted_window(true);
+  options_.set_output_type(SpectrogramCalculatorOptions::SQUARED_MAGNITUDE);
+
+  const std::vector<int> input_packet_sizes = {100};
+
+  InitializeGraph();
+  FillInputHeader();
+  // Setup packets with DC input (non-zero constant value).
+  SetupConstantInputPackets(input_packet_sizes);
+
+  MP_ASSERT_OK(Run());
+  // Shifted HANN window will be starting with non zero front, the exact window
+  // value match is tested in in audio/dsp/window_functions_test.cc, here just
+  // testing the first value..
+  CheckOutputHeadersAndTimestamps();
+  Matrix output_matrix = output().packets[0].Get<Matrix>();
+  // The magnitude of the 0th FFT bin (DC) should be sum(input.*window);
+  //  for an input identically 1.0, this is just sum(window).  The average
+  //  value of our Hann window is 0.5， and with a half-sample shift, it's still
+  //  sum to the same, which is sum(shifted_window) = sum(window), hence this is
+  //  the expected squared- magnitude output value in the DC bin for constant
+  //  input of 1.0.
+  EXPECT_FLOAT_EQ(output().packets[0].Get<Matrix>()(0, 0),
+                  expected_dc_squared_magnitude_);
 }
 
 void BM_ProcessDC(benchmark::State& state) {
