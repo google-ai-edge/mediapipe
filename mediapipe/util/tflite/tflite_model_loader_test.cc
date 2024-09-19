e3467701ebabd1dbe2fda4d125bf08022ac3c145
MediaPipe Team <mediapipe-team@google.com>
2024-09-18 17:30:37 -0700

    Migrate TfLiteModelLoader to use MP Resources.
    PiperOrigin-RevId: 676194539

--- mediapipe/util/tflite/tflite_model_loader_test.cc
+++ mediapipe/util/tflite/tflite_model_loader_test.cc
@@ -14,6 +14,7 @@
 #include "mediapipe/framework/legacy_calculator_support.h"
 #include "mediapipe/framework/port/gtest.h"
 #include "mediapipe/framework/port/status_matchers.h"
+#include "mediapipe/framework/resources.h"
 #include "mediapipe/framework/tool/tag_map_helper.h"
 #include "tensorflow/lite/test_util.h"
 
@@ -49,8 +50,10 @@ TEST_F(TfLiteModelLoaderTest, LoadFromPath) {
   // TODO: remove LegacyCalculatorSupport usage.
   LegacyCalculatorSupport::Scoped<CalculatorContext> scope(
       calculator_context_.get());
-  MP_ASSERT_OK_AND_ASSIGN(api2::Packet<TfLiteModelPtr> model,
-                          TfLiteModelLoader::LoadFromPath(model_path_));
+  std::unique_ptr<Resources> resources = CreateDefaultResources();
+  MP_ASSERT_OK_AND_ASSIGN(
+      api2::Packet<TfLiteModelPtr> model,
+      TfLiteModelLoader::LoadFromPath(*resources, model_path_));
   EXPECT_NE(model.Get(), nullptr);
 }
 
@@ -60,8 +63,10 @@ TEST_F(TfLiteModelLoaderTest, LoadFromPathRelativeToRootDir) {
   // TODO: remove LegacyCalculatorSupport usage.
   LegacyCalculatorSupport::Scoped<CalculatorContext> scope(
       calculator_context_.get());
-  MP_ASSERT_OK_AND_ASSIGN(api2::Packet<TfLiteModelPtr> model,
-                          TfLiteModelLoader::LoadFromPath(kModelFilename));
+  std::unique_ptr<Resources> resources = CreateDefaultResources();
+  MP_ASSERT_OK_AND_ASSIGN(
+      api2::Packet<TfLiteModelPtr> model,
+      TfLiteModelLoader::LoadFromPath(*resources, kModelFilename));
   EXPECT_NE(model.Get(), nullptr);
 }
 
@@ -69,9 +74,11 @@ TEST_F(TfLiteModelLoaderTest, LoadFromPathWithMmap) {
   // TODO: remove LegacyCalculatorSupport usage.
   LegacyCalculatorSupport::Scoped<CalculatorContext> scope(
       calculator_context_.get());
+  std::unique_ptr<Resources> resources = CreateDefaultResources();
   MP_ASSERT_OK_AND_ASSIGN(
       api2::Packet<TfLiteModelPtr> model,
-      TfLiteModelLoader::LoadFromPath(model_path_, /* try_mmap=*/true));
+      TfLiteModelLoader::LoadFromPath(*resources, model_path_,
+                                      /* try_mmap=*/true));
   ASSERT_NE(model.Get(), nullptr);
 
   // Tiny regression test for b/345663816.
