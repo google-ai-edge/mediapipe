8da23004504986bc73f02ea8fd940e21f67f97cb
Prianka Liz Kariat <prianka.kariat@codeandtheory.com>
2024-09-20 22:28:01 +0530

    Added utils for AVAudioFile

---
+++ mediapipe/tasks/ios/test/audio/core/utils/sources/AVAudioFile+TestUtils.m
@@ -0,0 +1,77 @@
+// Copyright 2024 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#import "mediapipe/tasks/ios/test/audio/core/utils/sources/AVAudioFile+TestUtils.h"
+
+#import "mediapipe/tasks/ios/test/audio/core/utils/sources/AVAudioPCMBuffer+TestUtils.h"
+
+@implementation MPPTimestampedAudioData
+
+- (instancetype)initWithAudioData:(MPPAudioData *)audioData
+          timestampInMilliseconds:(NSInteger)timestampInMilliseconds {
+  self = [super init];
+  if (self) {
+    _audioData = audioData;
+    _timestampInMilliseconds = timestampInMilliseconds;
+    ;
+  }
+  return self;
+}
+@end
+
+@implementation AVAudioFile (TestUtils)
+
++ (NSArray<MPPTimestampedAudioData *> *)
+    streamedAudioBlocksFromAudioFileWithInfo:(MPPFileInfo *)fileInfo
+                            modelSampleCount:(NSInteger)modelSampleCount
+                             modelSampleRate:(double)modelSampleRate {
+  AVAudioPCMBuffer *audioPCMBuffer =
+      [AVAudioPCMBuffer interleavedFloat32BufferFromAudioFileWithInfo:fileInfo];
+
+  // Calculates the maximum no: of samples possible in any one input chunk of audio samples by
+  // scaling the `modelSampleCount` to the sample rate of the input audio PCM Buffer. Each audio
+  // data in the returned array will have `intervalSize` of samples except for the last one. The
+  // last one will have all the remaining samples from the audio file.
+  AVAudioFrameCount intervalSize =
+      modelSampleCount * audioPCMBuffer.format.sampleRate / modelSampleRate;
+  .NSInteger currentPosition = 0;
+  NSInteger audioListCount =
+      (NSInteger)ceil((float)audioPCMBuffer.frameLength / (float)intervalSize);
+  NSMutableArray<MPPTimestampedAudioData *> *timestampedAudioDataList =
+      [NSMutableArray arrayWithCapacity:audioListCount];
+  while (currentPosition < audioPCMBuffer.frameLength) {
+    NSInteger lengthToBeLoaded = MIN(audioPCMBuffer.frameLength - currentPosition, intervalSize);
+    MPPAudioDataFormat *audioDataFormat =
+        [[MPPAudioDataFormat alloc] initWithChannelCount:audioPCMBuffer.format.channelCount
+                                              sampleRate:audioPCMBuffer.format.sampleRate];
+    MPPAudioData *audioData =
+        [[MPPAudioData alloc] initWithFormat:audioDataFormat
+                                 sampleCount:lengthToBeLoaded / audioDataFormat.channelCount];
+
+    MPPFloatBuffer *floatBuffer =
+        [[MPPFloatBuffer alloc] initWithData:audioPCMBuffer.floatChannelData + currentPosition
+                                      length:lengthToBeLoaded];
+    [audioData loadBuffer:floatBuffer offset:0 length:floatBuffer.length error:nil]
+
+        NSInteger timestampInMilliseconds =
+            currentPosition / audioPCMBuffer.format.sampleRate * 1000;
+    MPPTimestampedAudioData *timestampedAudioData =
+        [[MPPTimestampedAudioData alloc] initWithAudioData:audioData
+                                   timestampInMilliseconds:timestampInMilliseconds];
+    [timestampedAudioDataList addObject:timestampedAudioData];
+    currentPosition += lengthToBeLoaded;
+  }
+  return timestampedAudioDataList;
+}
+@end
