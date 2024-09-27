5e2226420d269798c2978c53e17b09cbc714c1ec
Prianka Liz Kariat <prianka.kariat@codeandtheory.com>
2024-09-27 18:04:03 +0530

    Added iOS audio classifier to cocoapods build

--- mediapipe/tasks/ios/build_ios_framework.sh
+++ mediapipe/tasks/ios/build_ios_framework.sh
@@ -56,12 +56,14 @@ case $FRAMEWORK_NAME in
     ;;
   "MediaPipeTasksText")
     ;;
+  "MediaPipeTasksAudio")
+    ;;
   "MediaPipeTasksGenAIC")
     ;;
   "MediaPipeTasksGenAI")
     ;;
   *)
-    echo "Wrong framework name. The following framework names are allowed: MediaPipeTasksText, MediaPipeTasksVision, MediaPipeTasksCommon, MediaPipeTasksGenAI, MediaPipeTasksGenAIC"
+    echo "Wrong framework name. The following framework names are allowed: MediaPipeTasksText, MediaPipeTasksVision, MediaPipeTasksAudio, MediaPipeTasksCommon, MediaPipeTasksGenAI, MediaPipeTasksGenAIC"
     exit 1
   ;;
 esac
@@ -99,7 +101,7 @@ function build_ios_frameworks_and_libraries {
   # the order of GBs. All iOS framework and library build commands for distribution via
   # CocoaPods must set --apple_generate_dsym=false inorder to shave down the binary size to
   # the order of a few MBs.
-
+  
   # Build Task Library xcframework.
   local FRAMEWORK_CQUERY_COMMAND="-c opt --config=ios_sim_device_fat --apple_generate_dsym=false --define OPENCV=source ${FULL_FRAMEWORK_TARGET}"
 
