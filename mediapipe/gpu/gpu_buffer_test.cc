14df499228813818a5e94860066917523697b3ce
MediaPipe Team <mediapipe-team@google.com>
2024-11-12 09:34:50 -0800

    Enable GpuBufferStorageAhwb ASYNC usage for use case: AhwbView write -> GlTextureView read
    * Adding AhwbView::SetUsageFence to allow for automatic async synchronization when getting GlTextureView
    * Adding AhwbView::GetWidthStepBytes to properly fill bytes
    * Adding GpuBufferStorageAhwb::Reuse to allow reusing storage for new writes
    * Exposing ReadTexture from gl_texture_buffer.cc to read GL texture data
    
    PiperOrigin-RevId: 695766930

--- mediapipe/gpu/gpu_buffer_test.cc
+++ mediapipe/gpu/gpu_buffer_test.cc
@@ -22,8 +22,6 @@
 #include "mediapipe/framework/tool/test_util.h"
 #include "mediapipe/gpu/gl_texture_buffer.h"
 #include "mediapipe/gpu/gl_texture_util.h"
-#include "mediapipe/gpu/gpu_buffer_storage_ahwb.h"
-#include "mediapipe/gpu/gpu_buffer_storage_image_frame.h"
 #include "mediapipe/gpu/gpu_test_base.h"
 #include "stb_image.h"
 #include "stb_image_write.h"
