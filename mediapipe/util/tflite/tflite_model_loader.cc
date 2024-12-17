d12c81054c2b1d5e2a1a67238faed033e5e8281d
MediaPipe Team <mediapipe-team@google.com>
2024-12-17 06:53:22 -0800

    Delegate memory-mapping the model file to the resource system
    PiperOrigin-RevId: 707080341

--- mediapipe/util/tflite/tflite_model_loader.cc
+++ mediapipe/util/tflite/tflite_model_loader.cc
@@ -15,75 +15,40 @@
 #include "mediapipe/util/tflite/tflite_model_loader.h"
 
 #include <memory>
+#include <optional>
 #include <string>
-#include <utility>
 
-#include "absl/log/absl_log.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/string_view.h"
 #include "mediapipe/framework/api2/packet.h"
-#include "mediapipe/framework/port/file_helpers.h"
 #include "mediapipe/framework/port/ret_check.h"
 #include "mediapipe/framework/port/status_macros.h"
 #include "mediapipe/framework/resources.h"
-#include "mediapipe/util/resource_util.h"
-#include "mediapipe/util/tflite/error_reporter.h"
-#include "tensorflow/lite/allocation.h"
 #include "tensorflow/lite/model_builder.h"
 
 namespace mediapipe {
 
-using ::mediapipe::util::tflite::ErrorReporter;
-using ::tflite::Allocation;
 using ::tflite::FlatBufferModel;
-using ::tflite::MMAPAllocation;
 
 absl::StatusOr<api2::Packet<TfLiteModelPtr>> TfLiteModelLoader::LoadFromPath(
     const Resources& resources, const std::string& path, bool try_mmap) {
   std::string model_path = path;
 
-  bool file_exists = file::Exists(model_path).ok();
-  if (!file_exists) {
-    // TODO: get rid of manual resolving with PathToResourceAsFile
-    // as soon as it's incorporated into GetResourceContents.
-    absl::StatusOr<std::string> resolved_model_path =
-        mediapipe::PathToResourceAsFile(model_path);
-    if (resolved_model_path.ok()) {
-      VLOG(2) << "Loading the model from " << model_path;
-      model_path = *std::move(resolved_model_path);
-      file_exists = true;
-    }
-  }
-
-  // Try to memory map file if available. Falls back to loading from buffer on
-  // error.
-  if (file_exists && try_mmap && MMAPAllocation::IsSupported()) {
-    ErrorReporter error_reporter;
-    std::unique_ptr<Allocation> allocation =
-        std::make_unique<MMAPAllocation>(model_path.c_str(), &error_reporter);
-
-    if (!error_reporter.HasError()) {
-      auto model = FlatBufferModel::BuildFromAllocation(std::move(allocation));
-      if (model) {
-        return api2::MakePacket<TfLiteModelPtr>(
-            model.release(), [](FlatBufferModel* model) { delete model; });
-      }
-    }
-
-    ABSL_LOG(WARNING) << "Failed to memory map model from path '" << model_path
-                      << "'; falling back to loading from buffer. Error: "
-                      << error_reporter.message();
-  }
-
   // Load model resource.
-  MP_ASSIGN_OR_RETURN(std::unique_ptr<Resource> model_resource,
-                      resources.Get(model_path));
+  MP_ASSIGN_OR_RETURN(
+      std::unique_ptr<Resource> model_resource,
+      resources.Get(
+          model_path,
+          Resources::Options{
+              .mmap_mode = try_mmap ? std::make_optional(MMapMode::kMMapOrRead)
+                                    : std::nullopt}));
   absl::string_view model_view = model_resource->ToStringView();
   auto model = FlatBufferModel::VerifyAndBuildFromBuffer(model_view.data(),
                                                          model_view.size());
 
-  RET_CHECK(model) << "Failed to load model from path " << model_path;
+  RET_CHECK(model) << "Failed to load model from path (resource ID) "
+                   << model_path;
   return api2::MakePacket<TfLiteModelPtr>(
       model.release(), [model_resource = model_resource.release()](
                            FlatBufferModel* model) mutable {
