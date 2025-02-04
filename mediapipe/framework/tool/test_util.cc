38f90e86eb19a0f2d88532b48c7fefa807455d33
MediaPipe Team <mediapipe-team@google.com>
2025-02-04 02:59:39 -0800

    No public description
    PiperOrigin-RevId: 723005897

--- mediapipe/framework/tool/test_util.cc
+++ mediapipe/framework/tool/test_util.cc
@@ -30,6 +30,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/str_join.h"
+#include "absl/strings/string_view.h"
 #include "absl/strings/substitute.h"
 #include "mediapipe/framework/calculator.pb.h"
 #include "mediapipe/framework/deps/file_path.h"
