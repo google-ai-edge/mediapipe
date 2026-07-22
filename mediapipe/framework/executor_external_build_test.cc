330b8529c2493ad391f78a2badaa5d6d9bfdd8a6
MediaPipe Team <mediapipe-team@google.com>
2026-07-21 22:04:51 -0700

    No public description
    PiperOrigin-RevId: 951887529

--- mediapipe/framework/executor_external_build_test.cc
+++ mediapipe/framework/executor_external_build_test.cc
@@ -45,7 +45,7 @@ class MyExecutor : public mediapipe::Executor {
 };
 
 MyExecutor::MyExecutor() {
-  thread_pool_ = absl::make_unique<mediapipe::ThreadPool>("my_executor", 1);
+  thread_pool_ = std::make_unique<mediapipe::ThreadPool>("my_executor", 1);
   thread_pool_->StartWorkers();
 }
 
